0af1e9bfeafae63a1d0b3640ff2d2ea503fc74dc
corepanic64 <isakulovtohir902@gmail.com>
2025-12-22 11:25:30 +0500

    implement exit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim())
+        let c1 = String::from(&command);
+        let c2 = String::from("exit");
+        if c1.trim() == c2.trim() {
+            break;
+        } else {
+            println!("{}: command not found", command.trim())
+        }
     }
 }
